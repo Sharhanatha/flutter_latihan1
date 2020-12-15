05ffb977a8ae8eb1c51c76c0800b71fe09f36c70
Sharhanatha <56475197+Sharhanatha@users.noreply.github.com>
2020-12-15 22:28:48 +0700

    First Commit

---
+++ lib/ui/widgets/widgets.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_latihan1/models/models.dart';
+import 'package:flutter_latihan1/ui/pages/pages.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+part 'productcard.dart';
